1b6e506be03a38377f930853afc94fd037392b36
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-02-20 16:26:48 +0530

    Update subnet.tf

--- terraform/wp_terraformscripts-master/subnet.tf
+++ terraform/wp_terraformscripts-master/subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "wp_subnet-1" {
   vpc_id                  = aws_vpc.demovpc.id
   cidr_block              = "10.0.1.0/24" 
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1a"
   tags = {
     Name = "Web Subnet-1"
   }
