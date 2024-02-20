94aa8bc03561194cc786f32d46d0af0f66ef24aa
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-02-20 16:36:21 +0530

    Update outputs.tf

--- terraform/wp_terraformscripts-master/outputs.tf
+++ terraform/wp_terraformscripts-master/outputs.tf
@@ -1,5 +1,5 @@
 #Getting Public instance IP address
 output "public_ip_adress" {
   description = "The Public IP of instance"
-  value       = aws_instance.wordpress8_ec2.public_ip
+  value       = aws_instance.terraform.public_ip
 }
