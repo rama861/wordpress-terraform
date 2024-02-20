b4b64683ff3a37d437ff9e2d1cb5d5e3f85b8357
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-02-20 16:15:06 +0530

    Update ec2.tf

--- terraform/wp_terraformscripts-master/ec2.tf
+++ terraform/wp_terraformscripts-master/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "wordpress8_ec2" {
   ami                         = "ami-0cf7b2f456cd5efd4"
   instance_type               = "t2.micro"
-  key_name                    = "new-key"
+  key_name                    = "docker-1"
   vpc_security_group_ids      = [aws_security_group.demosg.id]
   subnet_id                   = aws_subnet.wp_subnet-1.id
   associate_public_ip_address = "true"
