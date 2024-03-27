5be86a26af61b1e150e94da4647cd69d8ecac38e
rama861 <161824892+rama861@users.noreply.github.com>
2024-03-27 04:50:39 -0500

    Update ec2.tf

--- terraform/wp_terraformscripts-master/ec2.tf
+++ terraform/wp_terraformscripts-master/ec2.tf
@@ -1,8 +1,8 @@
 #Creating EC2 instance in Public subnet 
 resource "aws_instance" "terraform" {
-  ami                         = "ami-0440d3b780d96b29d"
+  ami                         = "ami-000c0df09737657b6"
   instance_type               = "t2.micro"
-  key_name                    = "docker-1"
+  key_name                    = "cheran"
   vpc_security_group_ids      = [aws_security_group.demosg.id]
   subnet_id                   = aws_subnet.wp_subnet-1.id
   associate_public_ip_address = "true"
